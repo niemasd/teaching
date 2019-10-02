a4b11ec0705007ef740d43778c76e098e98ddd94
Niema Moshiri <niemamoshiri@gmail.com>
2019-10-02 11:00:27 -0700

    Added output file feature

--- codePost/codePost_points_to_roster_order.py
+++ codePost/codePost_points_to_roster_order.py
@@ -3,6 +3,7 @@
 Given a codePost grade/score report, reorder the points to be in the same order as the given roster file
 Niema Moshiri 2019
 '''
+from os.path import isfile
 
 # main function
 if __name__ == "__main__":
@@ -10,7 +11,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-r', '--roster', required=True, type=str, help="Roster (TSV) (Last, First, Email, PID, Stepik, iClicker, Grade ID)")
     parser.add_argument('-g', '--grades', required=True, type=str, help="codePost Grade/Score Report (CSV) (Email, Points)")
+    parser.add_argument('-o', '--output', required=False, type=str, default="stdout", help="Output File")
     args = parser.parse_args()
+    if args.output == 'stdout':
+        from sys import stdout as outfile
+    else:
+        assert not isfile(args.output), "Output file exists: %s" % args.output
+        outfile = open(args.output,'w')
 
     # load codePost points
     points = dict()
@@ -29,5 +36,5 @@ if __name__ == "__main__":
         last,first,email,pid,stepik,iclicker = [v.strip() for v in l.strip().split('\t')][:6]
         if email not in points:
             points[email] = 0
-        print('\t'.join([v.strip() for v in l.strip().split('\t')][:6]), end='\t')
-        print(points[email])
+        outfile.write('\t'.join([v.strip() for v in l.strip().split('\t')][:6]))
+        outfile.write('\t%d\n' % points[email])
