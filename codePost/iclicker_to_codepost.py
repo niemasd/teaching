daafe5f21a544e8067e72163e33af0e7060b3306
Niema Moshiri <niemamoshiri@gmail.com>
2019-10-04 19:08:21 -0700

    Ignore empty lines

--- codePost/iclicker_to_codepost.py
+++ codePost/iclicker_to_codepost.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     print("Parsing roster: %s" % args.roster)
     email_to_iclicker = dict()
     for l in open(args.roster):
-        if len(l.strip()) == 0 or l.startswith("Last Name\t"):
+        if l.startswith("Last Name\t"):
             continue
         last,first,email,pid,stepik,iclicker = [v.strip() for v in l.strip().split('\t')][:6]
         assert email == '' or email not in email_to_iclicker, "Duplicate Email: %s" % email
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     # parse iClicker report
     print("Parsing iClicker participation report: %s" % args.iclicker)
     for l in open(args.iclicker):
-        if l.startswith('Last Name'):
+        if len(l.strip()) == 0 or l.startswith('Last Name'):
             continue
         try:
             last,first,pid,iclicker,p = [v.strip().replace('"','').replace("'",'') for v in l.split(',')]
