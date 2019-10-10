3cd6b6b316462ffb3c880ae75b347072da377e36
Niema Moshiri <niemamoshiri@gmail.com>
2019-10-10 15:53:02 -0700

    Fixed bug

--- MOSS/moss_links_to_zip.py
+++ MOSS/moss_links_to_zip.py
@@ -24,11 +24,11 @@ if __name__ == "__main__":
     links = load(infile)
     emails = sorted(links.keys())
     outzip = ZipFile(args.output, mode='w', compression=ZIP_DEFLATED)
-    num_pairs = int(len(emails)*(len(emails)-1)/2)
+    num_pairs = int(len(emails)*(len(emails)-1)/2); pair_num = 1
     for i in range(len(emails)-1):
         for j in range(i+1, len(emails)):
             if emails[j] in links[emails[i]]:
                 folder = "%s,%s" % (emails[i], emails[j])
                 for url in links[emails[i]][emails[j]]:
-                    outzip.writestr("%s/%s" % (folder, url.split('/')[-2]), urlopen(url).read().decode())
-            print("Successfully downloaded student pair %d of %d" % (len(emails)*i+j+1, num_pairs), end='\r')
+                    outzip.writestr("%s/%s.html" % (folder, url.split('/')[-2]), urlopen(url).read().decode())
+            print("Successfully downloaded student pair %d of %d" % (pair_num, num_pairs), end='\r'); pair_num += 1
