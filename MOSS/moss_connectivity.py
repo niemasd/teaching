d05e6291c1d782f032f50fc7021f0f7f0b9ac2ea
Niema Moshiri <niemamoshiri@gmail.com>
2019-10-10 14:32:14 -0700

    Added script to generate MOSS linkage from multiple reports

---
+++ MOSS/moss_connectivity.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+'''
+Given a list of MOSS URLs, create a JSON of the links between pairs of students
+Files given to MOSS should be in a folder structure "email_address/file"
+Niema Moshiri 2019
+'''
+from json import dump
+from urllib.request import urlopen
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-i', '--input', required=False, type=str, default='stdin', help="MOSS URLs (one per line)")
+    parser.add_argument('-o', '--output', required=False, type=str, default='stdout', help="Output File (JSON)")
+    args = parser.parse_args()
+    if args.input == 'stdin':
+        from sys import stdin as infile
+    else:
+        infile = open(args.input)
+    if args.output == 'stdout':
+        from sys import stdout as outfile
+    else:
+        outfile = open(args.output,'w')
+
+    # parse MOSS URLs
+    links = dict() # links[email1][email2] is the URL of the match between email1 and email2
+    for url in infile:
+        page = urlopen(url).read().decode()
+        susp = [l.strip() for l in page.replace('\n','').replace('<TR>','\n').replace('</TABLE>','\n').splitlines() if '/match' in l]
+        for link in susp:
+            moss_url = link.split('</A>')[0].split('HREF')[1].split('"')[1].strip()
+            email1 = link.split('</A>')[0].split('HREF')[1].split('>')[1].split('/')[0].strip()
+            email2 = link.split('</A>')[1].split('HREF')[1].split('>')[1].split('/')[0].strip()
+            if email1 not in links:
+                links[email1] = dict()
+            if email2 not in links:
+                links[email2] = dict()
+            links[email1][email2] = links[email2][email1] = moss_url
+    dump(links,outfile)
