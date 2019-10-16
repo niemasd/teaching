65e052239686b71fb08a5500c051bf925e18f9f5
Niema Moshiri <niemamoshiri@gmail.com>
2019-10-16 14:58:21 -0700

    Added script to bulk-upload Stepik lesson submission folder to MOSS

---
+++ MOSS/submission_folder_to_MOSS.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+if [ "$#" -ne 2 ] ; then
+    echo "USAGE: $0 <submission_folder> <language>"; exit 1
+fi
+nums=$(ls -l $1/*/*.java | rev | cut -d'/' -f1 | rev | sort | uniq)
+
+echo -n "Submitting code for $(echo $nums | wc -l) problems to MOSS... "
+for f in $nums ; do moss.pl -l $2 $1/*/$f > $f.log 2>/dev/null & done
+echo "done"
+
+echo -n "Waiting for jobs to finish... "
+for job in $(jobs -p) ; do wait $job ; done
+echo "done"
