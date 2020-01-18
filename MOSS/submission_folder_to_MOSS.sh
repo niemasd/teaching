dee259a99097e64b4282c3061ac6628ac73d3d29
Niema Moshiri <niemamoshiri@gmail.com>
2020-01-18 10:39:06 -0800

    Updated moss script

--- MOSS/submission_folder_to_MOSS.sh
+++ MOSS/submission_folder_to_MOSS.sh
@@ -2,10 +2,11 @@
 if [ "$#" -ne 2 ] ; then
     echo "USAGE: $0 <submission_folder> <language>"; exit 1
 fi
-nums=$(ls -l $1/*/*.java | rev | cut -d'/' -f1 | rev | sort | uniq)
+cd $1
+nums=$(ls -l */*.java | rev | cut -d'/' -f1 | rev | sort | uniq)
 
 echo -n "Submitting code for $(echo $nums | tr ' ' '\n' | wc -l) problems to MOSS... "
-for f in $nums ; do moss.pl -l $2 $1/*/$f > $f.log 2>/dev/null & done
+for f in $nums ; do moss.pl -l $2 */$f > $f.log 2>/dev/null & done
 echo "done"
 
 echo -n "Waiting for jobs to finish... "
