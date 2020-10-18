ec31cb05303d4bf8abb1ac83042c5670460ac0be
Niema Moshiri <niemamoshiri@gmail.com>
2020-10-18 07:54:04 -0700

    Hopefully added ability to manually map students from Ed to Canvas (e.g. Extension students)

--- Ed/quiz_responses_to_canvas.py
+++ Ed/quiz_responses_to_canvas.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     parser.add_argument('-q', '--ed_quiz_responses', required=True, type=str, help="Ed Quiz Responses (CSV)")
     parser.add_argument('-qc', '--ed_quiz_columns', required=True, type=str, help="Column(s) of Ed Quiz Responses for each graded question (0-based indexing)")
     parser.add_argument('-o', '--output', required=False, type=str, default='stdout', help="Output file to be imported into Canvas")
+    parser.add_argument('-m', '--mapping', required=False, type=str, default=None, help="Manual Ed to Canvas mapping (TSV, col0=Ed, col1=Canvas)")
     args = parser.parse_args()
     if not isfile(args.canvas_gradebook):
         raise ValueError("File not found: %s" % args.canvas_gradebook)
@@ -27,6 +28,15 @@ if __name__ == "__main__":
         from sys import stdout; args.output = stdout
     else:
         args.output = open(args.output, 'w')
+    if args.mapping is None:
+        ed2canvas = dict(); canvas2ed = dict()
+    else:
+        if not isfile(args.mapping):
+            raise ValueError("File not found: %s" % args.mapping)
+        lines = [[v.strip() for v in l.split('\t')] for l in open(args.mapping) if len(l.strip()) != 0]
+        ed2canvas = {l[0]:l[1] for l in lines}; canvas2ed = {l[1]:l[0] for l in lines}
+        if len(lines) != len(ed2canvas) or len(lines) != len(canvas2ed):
+            raise ValueError("Ed to Canvas mapping file had duplicate items")
 
     # parse Ed columns
     ed_columns = list()
@@ -73,8 +83,9 @@ if __name__ == "__main__":
         else:
             if sis_login_ID in quiz_score:
                 curr_score = quiz_score[sis_login_ID]
+            else if sis_login_ID in canvas2ed and canvas2ed[sis_login_ID] in quiz_score:
+                curr_score = quiz_score[canvas2ed[sis_login_ID]]
             else:
                 stderr.write("CANVAS STUDENT NOT FOUND IN ED: %s\n" % sis_login_ID); curr_score = 0
             out_csv.writerow([student, ID, sis_user_ID, sis_login_ID, section, str(curr_score)])
-
     args.output.close()
