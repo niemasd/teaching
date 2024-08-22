ee22ded34c25a1131fc0ded2a1eebec7a69b8349
Niema Moshiri <niemamoshiri@gmail.com>
2024-08-22 19:10:09 +0000

    Progress

--- PrairieLearn/run_test.py
+++ PrairieLearn/run_test.py
@@ -1,11 +1,29 @@
 #! /usr/bin/env python3
 '''
-Run a Python unittest grading script (test.py) in PrairieLearn. Assumes the following folder structure:
+Run a Python unittest / Pytest grading script (test.py) in PrairieLearn. Assumes the following folder structure:
+
+/grade/results/results.json             # test output formatted for PrairieLearn
+/grade/serverFilesCourse/run_test.py    # this script
+/grade/student/                         # folder containing student files to grade
+/grade/tests/test*.py                   # Python unittest / Pytest scripts to run
+
+https://prairielearn.readthedocs.io/en/latest/externalGrading/
 '''
 
 # imports
 from pathlib import Path
 
-# constants
-SELF_DIR_O = Path(__file__).parent
-SELF_DIR = SELF_DIR_O.resolve()
+# run grader
+if __name__ == "__main__":
+    # determine and check paths
+    GRADE_DIR = Path(__file__).parent.parent
+    RESULTS_DIR = GRADE_DIR / 'results'
+    STUDENT_DIR = GRADE_DIR / 'student'
+    TESTS_DIR = GRADE_DIR / 'tests'
+    for d in [GRADE_DIR, STUDENT_DIR, TESTS_DIR]:
+        assert d.is_dir(), "Directory not found: %s" % d
+    RESULTS_DIR.mkdir(exist_ok=True)
+
+    # run tests
+    for test_py in TESTS_DIR.glob('test*.py'):
+        print(test_py) # TODO
